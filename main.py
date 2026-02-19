00a549bd3be1b1d4bcf1d9af8a9128e03982803d
HardikLovesTech <hardikrunwal123@gmail.com>
2026-02-20 00:04:02 +0530

    Add CORS for Vercel frontend

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from backend.Simulation import HawkesProcess
 from backend.FitModel import FitHawkesModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "https://lambdalab.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class SimulationRequest(BaseModel):
     Mu: float
